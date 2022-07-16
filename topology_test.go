6a0b8a2063a6c9a298144a7808d1c0fdbc9479f6
Jeadie <jackeadie@duck.com>
2022-07-16 12:40:01 +1000

    add tests for topology transforms

---
+++ topology_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TopologyTestCase struct {
+	x, y, dx, dy, n int
+	expectedX, expectedY int
+}
+
+type TransformTestSet struct {
+	tests []TopologyTestCase
+	topology string
+}
+
+func RunCase(test TopologyTestCase, fn TopologyTransformation) error {
+	nx, ny:= fn(test.x, test.y, test.dx, test.dy, test.n)
+	if nx != test.expectedX || ny != test.expectedY  {
+		return fmt.Errorf(
+			"incorrect output for (%d+%d, %d+%d) on size %d. Expected (%d, %d), output (%d, %d)",
+			test.x, test.dx, test.y, test.dy, test.n, test.expectedX, test.expectedY, nx, ny)
+	}
+	return nil
+}
+
+func TestTopologyTransformation(t *testing.T) {
+	tests := []TransformTestSet{{
+		topology: Bordered,
+		tests:    []TopologyTestCase{
+			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		},
+	},{
+		topology: Torus,
+		tests:    []TopologyTestCase{
+			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: 4, expectedY: 0},
+			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: 0, expectedY: 4},
+			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
+			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		},
+	},{
+		topology: KleinBottle,
+		tests:    []TopologyTestCase{
+			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		},
+	},{
+		topology: ProjectivePlane,
+		tests:    []TopologyTestCase{
+			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		},
+	},{
+		topology: Sphere,
+		tests:    []TopologyTestCase{
+			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: 4, expectedY: 4},
+			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: 4, expectedY: 4},
+			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
+
+			{ x: 3, y: 4, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
+			{ x: 4, y: 3, dx: 1, dy: 0, n: 5, expectedX: 1, expectedY: 0},
+			{ x: 4, y: 4, dx: 1, dy: 1, n: 5, expectedX: 0, expectedY: 0},
+
+			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		},
+	},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.topology, func(t *testing.T) {
+			fn := GetTransformation(testCase.topology)
+			for _, test := range testCase.tests {
+				err := RunCase(test, fn)
+				if err != nil {
+					t.Errorf(err.Error())
+				}
+			}
+		})
+	}
+}
