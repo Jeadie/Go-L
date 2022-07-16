339ae53d20ec8026487319ad483e5b9d9970c3f7
Jeadie <jackeadie@duck.com>
2022-07-17 09:43:35 +1000

    projective plane and klein bottle

--- topology.go
+++ topology.go
@@ -3,12 +3,13 @@ package main
 const (
 	Bordered        string = "BORDERED"
 	Torus                  = "TORUS"
-	Sphere				   = "SPHERE"
+	Sphere                 = "SPHERE"
 	KleinBottle            = "KLEIN_BOTTLE"
 	ProjectivePlane        = "PROJECTIVE_PLANE"
 )
 
 var ALLOWED_TOPOLOGIES = []string{Bordered, Torus, KleinBottle, ProjectivePlane, Sphere}
+
 const DefaultTopology = Bordered
 
 // Returns true iff x is an allowed topology.
@@ -45,7 +46,7 @@ func GetTransformation(topology string) TopologyTransformation {
 func TranslateBordered(x, y int, dx, dy int, n int) (int, int) {
 	// Efficient non-boundary case return.
 	if x+dx >= 0 && x+dx < n && y+dy >= 0 && y+dy < n {
-		return x+dx, y+dy
+		return x + dx, y + dy
 	}
 
 	return -1, -1
@@ -54,46 +55,65 @@ func TranslateBordered(x, y int, dx, dy int, n int) (int, int) {
 func TranslateTorus(x, y int, dx, dy int, n int) (int, int) {
 	// Efficient non-boundary case return.
 	if x+dx >= 0 && x+dx < n && y+dy >= 0 && y+dy < n {
-		return x+dx, y+dy
+		return x + dx, y + dy
 	}
 
-	return mod(x + dx, n), mod(y + dy, n)
+	return mod(x+dx, n), mod(y+dy, n)
 }
 
 func TranslateKleinBottle(x, y int, dx, dy int, n int) (int, int) {
 	// Efficient non-boundary case return.
 	if x+dx >= 0 && x+dx < n && y+dy >= 0 && y+dy < n {
-		return x+dx, y+dy
+		return x + dx, y + dy
 	}
 
-	ny := y
-	if (y+dy) < 0 || (y+dy) >= n {
-		ny = mod(-1 * (y + dy), n)
+	ny := y + dy
+	nx := x + dx
+
+	// Top boundary reverse x co-ordinate
+	if y+dy == n {
+		nx = n - (x + dx)
 	}
-	return mod(x + dx, n), ny
+
+	if (y + dy) == n {
+		ny = mod(n-(y+dy), n)
+	}
+	return mod(nx, n), mod(ny, n)
 }
 
 func TranslateProjectivePlane(x, y int, dx, dy int, n int) (int, int) {
 	// Efficient non-boundary case return.
 	if x+dx >= 0 && x+dx < n && y+dy >= 0 && y+dy < n {
-		return x+dx, y+dy
+		return x + dx, y + dy
 	}
 
-	nx := x
-	ny := y
-	if (y+dy) < 0 || (y+dy) >= n {
-		ny = mod(-1 * (y + dy), n)
+	nx := x + dx
+	ny := y + dy
+
+	// It appears (0, 5) & (5,0) cannot be mapped properly.
+	// We shall map (0, 5) & (5,0) to (-1, -1)
+	if (nx == 0 && ny == n) || (nx == n && ny == 0) {
+		return -1, -1
 	}
-	if (x+dx) < 0 || (x+dx) >= n {
-		nx = mod(-1 * (x + dx), n)
+
+	// Border line conditions
+	if ny >= n || ny < 0 {
+		ny = mod(ny, n)
+		nx = n - x
 	}
-	return nx, ny
+	if nx >= n || nx < 0 {
+		nx = mod(nx, n)
+		ny = n - y
+	}
+
+	// Border line might require two mappings
+	return TranslateProjectivePlane(nx, ny, 0, 0, n) // mod(nx, n), mod(ny, n)
 }
 
 func TranslateSphere(x, y int, dx, dy int, n int) (int, int) {
 	// Efficient non-boundary case return.
 	if x+dx >= 0 && x+dx < n && y+dy >= 0 && y+dy < n {
-		return x+dx, y+dy
+		return x + dx, y + dy
 	}
 
 	//
@@ -120,7 +140,7 @@ func TranslateSphere(x, y int, dx, dy int, n int) (int, int) {
 }
 
 // Reflect the point (x,y) back into the square bound by (0, 0), (n, n)
-func reflectSquare(x,y, n int) (int, int) {
+func reflectSquare(x, y, n int) (int, int) {
 	return reflectSquareCoordinate(x, n), reflectSquareCoordinate(y, n)
 }
 
@@ -128,14 +148,14 @@ func reflectSquareCoordinate(a, n int) int {
 	if a < 0 {
 		return -a
 	} else if a > n {
-		return n - (a-n)
+		return n - (a - n)
 	}
 	return a
 }
 
 // Reflect  point (x,y) across y=n-x
 func reflectSphere(x, y, n int) (int, int) {
-	return n-y, n-x
+	return n - y, n - x
 }
 
 // mod operation that handles negative number properly.
--- topology_test.go
+++ topology_test.go
@@ -6,18 +6,18 @@ import (
 )
 
 type TopologyTestCase struct {
-	x, y, dx, dy, n int
+	x, y, dx, dy, n      int
 	expectedX, expectedY int
 }
 
 type TransformTestSet struct {
-	tests []TopologyTestCase
+	tests    []TopologyTestCase
 	topology string
 }
 
 func RunCase(test TopologyTestCase, fn TopologyTransformation) error {
-	nx, ny:= fn(test.x, test.y, test.dx, test.dy, test.n)
-	if nx != test.expectedX || ny != test.expectedY  {
+	nx, ny := fn(test.x, test.y, test.dx, test.dy, test.n)
+	if nx != test.expectedX || ny != test.expectedY {
 		return fmt.Errorf(
 			"incorrect output for (%d+%d, %d+%d) on size %d. Expected (%d, %d), output (%d, %d)",
 			test.x, test.dx, test.y, test.dy, test.n, test.expectedX, test.expectedY, nx, ny)
@@ -28,48 +28,71 @@ func RunCase(test TopologyTestCase, fn TopologyTransformation) error {
 func TestTopologyTransformation(t *testing.T) {
 	tests := []TransformTestSet{{
 		topology: Bordered,
-		tests:    []TopologyTestCase{
-			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		tests: []TopologyTestCase{
+			{x: 0, y: 0, dx: -1, dy: 0, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: 0, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
 		},
-	},{
+	}, {
 		topology: Torus,
-		tests:    []TopologyTestCase{
-			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: 4, expectedY: 0},
-			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: 0, expectedY: 4},
-			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
-			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		tests: []TopologyTestCase{
+			{x: 0, y: 0, dx: -1, dy: 0, n: 5, expectedX: 4, expectedY: 0},
+			{x: 0, y: 0, dx: 0, dy: -1, n: 5, expectedX: 0, expectedY: 4},
+			{x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
+			{x: 0, y: 0, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
 		},
-	},{
+	}, {
 		topology: KleinBottle,
-		tests:    []TopologyTestCase{
-			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		tests: []TopologyTestCase{
+			// Boundary Conditions
+			{x: 5, y: 1, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 1},
+			{x: 3, y: 5, dx: 0, dy: 0, n: 5, expectedX: 2, expectedY: 0},
+
+			// Corner conditions
+			{x: 5, y: 0, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 0},
+			{x: 0, y: 5, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 0},
+			{x: 5, y: 5, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 0},
+
+			// Out of bound conditions
+			{x: 0, y: 0, dx: -1, dy: 0, n: 5, expectedX: 4, expectedY: 0},
+			{x: 0, y: 0, dx: 0, dy: -1, n: 5, expectedX: 0, expectedY: 4},
+			{x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
+			{x: 0, y: 0, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
 		},
-	},{
+	}, {
 		topology: ProjectivePlane,
-		tests:    []TopologyTestCase{
-			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
-			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+		tests: []TopologyTestCase{
+
+			// Boundary conditions
+			{x: 3, y: 5, dx: 0, dy: 0, n: 5, expectedX: 2, expectedY: 0},
+			{x: 5, y: 3, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 2},
+
+			// Corner conditions
+			{x: 5, y: 5, dx: 0, dy: 0, n: 5, expectedX: 0, expectedY: 0},
+
+			// These are unmappable, default to (-1, -1)
+			{x: 5, y: 0, dx: 0, dy: 0, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 5, dx: 0, dy: 0, n: 5, expectedX: -1, expectedY: -1},
+
+			// Out of bound conditions
+			{x: 0, y: 0, dx: -1, dy: 0, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: 0, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: -1, expectedY: -1},
+			{x: 0, y: 0, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
 		},
-	},{
+	}, {
 		topology: Sphere,
-		tests:    []TopologyTestCase{
-			{ x: 0, y: 0, dx: -1, dy: 0,  n: 5, expectedX: 1, expectedY: 0},
-			{ x: 0, y: 0, dx: 0,  dy: -1, n: 5, expectedX: 0, expectedY: 1},
-			{ x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
+		tests: []TopologyTestCase{
+			{x: 0, y: 0, dx: -1, dy: 0, n: 5, expectedX: 1, expectedY: 0},
+			{x: 0, y: 0, dx: 0, dy: -1, n: 5, expectedX: 0, expectedY: 1},
+			{x: 0, y: 0, dx: -1, dy: -1, n: 5, expectedX: 4, expectedY: 4},
 
-			{ x: 3, y: 4, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 3},
-			{ x: 4, y: 3, dx: 1, dy: 0, n: 5, expectedX: 3, expectedY: 0},
-			{ x: 4, y: 4, dx: 1, dy: 1, n: 5, expectedX: 0, expectedY: 0},
+			{x: 3, y: 4, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 3},
+			{x: 4, y: 3, dx: 1, dy: 0, n: 5, expectedX: 3, expectedY: 0},
+			{x: 4, y: 4, dx: 1, dy: 1, n: 5, expectedX: 0, expectedY: 0},
 
-			{ x: 0, y: 0, dx: 0,  dy: 1,  n: 5, expectedX: 0,  expectedY: 1},
+			{x: 0, y: 0, dx: 0, dy: 1, n: 5, expectedX: 0, expectedY: 1},
 		},
 	},
 	}
